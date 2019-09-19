3623e83177e5f211b3b59820400040f394df1fe7
Michael Mstoi <michael.mstoi@fi.abb.com>
2019-09-19 12:06:43 +0300

    initial version

---
+++ Nucleo_L476RG_PR_5_Timer2/main.c
@@ -0,0 +1,75 @@
+#include "stm32L476xx.h"
+#include "stm32L4xx_hal.h"
+#include "stm32L4xx_hal_gpio.h"
+
+//#include "EventRecorder.h"
+
+#include <cmsis_os.h>
+#include "Board_led.h"
+
+
+
+osThreadId main_ID;
+osThreadId led_ID1, led_ID2;
+
+void led_thread1(void const *arg);
+void led_thread2(void const *arg);
+
+void led_switcher(void const *arg);
+
+//osThreadDef(led_thread2, osPriorityAboveNormal, 1, 0);
+//osThreadDef(led_thread1, osPriorityNormal, 1, 0);
+
+	osThreadDef(led_switcher, osPriorityNormal, 2, 0);
+
+int main(void)
+{	
+	
+	LED_Initialize();
+	
+	//main_ID = osThreadGetId();
+  //osThreadSetPriority(main_ID, osPriorityAboveNormal);
+	
+	//led_ID2 = osThreadCreate(osThread(led_thread2), NULL);
+  //led_ID1 = osThreadCreate(osThread(led_thread1), NULL);
+	
+	led_ID1 = osThreadCreate(osThread(led_switcher), (void*) 0ul);
+  led_ID2 = osThreadCreate(osThread(led_switcher), (void*) 2ul);
+	os_idle_demon();
+	//while(1);
+}
+
+
+void led_switcher(void const *arg)
+{
+	for(;;){
+		LED_On((uint32_t)arg);
+		osDelay(500);
+		LED_Off((uint32_t)arg);
+		osDelay(500);
+	}
+
+}
+
+
+void led_thread1(void const *arg)
+{
+	for(;;)
+	{
+		LED_On(0);
+		osDelay(500);
+		LED_Off(0);
+		osDelay(500);
+	}
+}
+void led_thread2(void const *arg)
+{
+	for(;;)
+	{
+		LED_On(0);
+		osDelay(500);
+		LED_Off(0);
+		osDelay(500);
+	}
+}
+
